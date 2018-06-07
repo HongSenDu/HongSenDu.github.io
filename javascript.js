b2b22064bad5d0c09306fa8b7366f3ca3472161d
Hong Sen Du <hongsendu@gmail.com>
2018-06-06 22:22:23 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -9,6 +9,12 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  $('.burgers').on("click",function(){
+    $("nav li").toggleClass("open");
+  });
+});
+
 
 particlesJS.load('background','particles.json',function(){
 console.log('particles.json loaded...')
